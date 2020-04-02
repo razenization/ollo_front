40ad228022632d9e7b03fbdb3d56e885945e6a8d
razenization <reactionnnnn@gmail.com>
2020-04-02 19:50:09 +0300

    Finished dota prematch page, added graph script using jquery, created some custom elements. Next up is wrapping to mobile devices and implementing some javascript logic.

--- dist/assets/other/prematch.js
+++ dist/assets/other/prematch.js
@@ -5,8 +5,7 @@
 				$('.mutual__bg').addClass('active');
 				$(this).parents('.mutual__switcher-choices').addClass('hover');
 			}, function () {
-		// $(this).parents('.mutual__switcher-choices').removeClass('hover');
-	});
+			});
 
 		$('.mutual__bg').hover(
 			function() {
@@ -18,32 +17,25 @@
 				$(this).parents('.mutual__switcher-choices').removeClass('hover');
 			});
 
-		/* Look for any elements with the class "custom-select": */
+		// Custom selectbox script (modified)
 		x = document.getElementsByClassName("custom-select");
 		for (i = 0; i < x.length; i++) {
 			selElmnt = x[i].getElementsByTagName("select")[0];
-			/* For each element, create a new DIV that will act as the selected item: */
 			a = document.createElement("DIV");
 			a.setAttribute("class", "select-selected");
 			a.innerHTML = selElmnt.options[selElmnt.selectedIndex].innerHTML;
 			$(a).html('<img src="assets/img/bookies/ggbet.svg" alt="" style="margin-right: 5px;">' + selElmnt.options[selElmnt.selectedIndex].innerHTML);
 			x[i].appendChild(a);
-			/* For each element, create a new DIV that will contain the option list: */
 			b = document.createElement("DIV");
 			b.setAttribute("class", "select-items select-hide");
 			for (j = 1; j < selElmnt.length; j++) {
-				/* For each option in the original select element,
-				create a new DIV that will act as an option item: */
 				c = document.createElement("DIV");
 				$(c).css({
 					'display': 'flex',
 					'align-items': 'center'
 				});
 				$(c).html('<img src="assets/img/bookies/ggbet.svg" alt="" style="margin-right: 5px;">' + selElmnt.options[j].innerHTML);
-				// c.innerHTML = selElmnt.options[j].innerHTML;
 				c.addEventListener("click", function(e) {
-					/* When an item is clicked, update the original select box,
-					and the selected item: */
 					var y, i, k, s, h;
 					s = this.parentNode.parentNode.getElementsByTagName("select")[0];
 					h = this.parentNode.previousSibling;
@@ -65,37 +57,68 @@
 			}
 			x[i].appendChild(b);
 			a.addEventListener("click", function(e) {
-		/* When the select box is clicked, close any other select boxes,
-		and open/close the current select box: */
-		e.stopPropagation();
-		closeAllSelect(this);
-		this.nextSibling.classList.toggle("select-hide");
-		this.classList.toggle("select-arrow-active");
-	});
+				e.stopPropagation();
+				closeAllSelect(this);
+				this.nextSibling.classList.toggle("select-hide");
+				this.classList.toggle("select-arrow-active");
+			});
 		}
 
 		function closeAllSelect(elmnt) {
-	  /* A function that will close all select boxes in the document,
-	  except the current select box: */
-	  var x, y, i, arrNo = [];
-	  x = document.getElementsByClassName("select-items");
-	  y = document.getElementsByClassName("select-selected");
-	  for (i = 0; i < y.length; i++) {
-	  	if (elmnt == y[i]) {
-	  		arrNo.push(i)
-	  	} else {
-	  		y[i].classList.remove("select-arrow-active");
-	  	}
-	  }
-	  for (i = 0; i < x.length; i++) {
-	  	if (arrNo.indexOf(i)) {
-	  		x[i].classList.add("select-hide");
-	  	}
-	  }
-	}
+			var x, y, i, arrNo = [];
+			x = document.getElementsByClassName("select-items");
+			y = document.getElementsByClassName("select-selected");
+			for (i = 0; i < y.length; i++) {
+				if (elmnt == y[i]) {
+					arrNo.push(i)
+				} else {
+					y[i].classList.remove("select-arrow-active");
+				}
+			}
+			for (i = 0; i < x.length; i++) {
+				if (arrNo.indexOf(i)) {
+					x[i].classList.add("select-hide");
+				}
+			}
+		}
+		document.addEventListener("click", closeAllSelect);
 
-/* If the user clicks anywhere outside the select box,
-then close all select boxes: */
-document.addEventListener("click", closeAllSelect);
-	}); // end of document ready
-})(jQuery); // end of jQuery name space
+		// Graphs
+		var dials = $(".dial");
+
+		for (i=0; i < dials.length; i++) {
+			var width = (typeof $(dials[i]).attr("data-width") != 'undefined') ? Math.round($(dials[i]).attr("data-width")) : 80;
+			var percent = (Number($(dials[i]).attr('data-percent')) > 0 && Number($(dials[i]).attr('data-percent')) < 100) ? Math.round(Number($(dials[i]).attr('data-percent')) * 10)/10 : 0;
+			var lineWidth = (typeof $(dials[i]).attr("data-lineWidth") != 'undefined') ? Number($(dials[i]).attr("data-lineWidth")) : width / 10;
+			if (lineWidth >= width) lineWidth = width + 1;
+			var size = width + lineWidth;
+			var lineRound = (typeof $(dials[i]).attr("data-lineRound") != 'undefined') ? true : false;
+			var borderColor = $(dials[i]).css("border-color");
+			var color = $(dials[i]).css("color");
+
+			$(dials[i]).css({"width": size + 'px', "height": size + 'px', "font-size": Math.floor((width-lineWidth) / 4) + 'px'});
+			$(dials[i]).prepend('<canvas id="dial' + i + '" width="' + size + '" height="' + size + '"></canvas>');
+
+			// $("p", dials[i]).css({"line-height": size + 'px'});
+			var canvas = document.getElementById("dial" + i);
+			var context = canvas.getContext("2d");
+			var radian = 2 * Math.PI * percent/100;
+
+			context.arc(width/2+lineWidth/2, width/2+lineWidth/2, width/2, 0, 2*Math.PI, false);
+			context.lineWidth = lineWidth;
+			context.strokeStyle = borderColor;
+			context.stroke();
+
+			context.beginPath();
+			context.arc(width/2+lineWidth/2, width/2+lineWidth/2, width/2, Math.PI, radian/2 + Math.PI, false);
+			context.strokeStyle = color;
+			context.stroke();
+			context.beginPath();
+			context.arc(width/2+lineWidth/2, width/2+lineWidth/2, width/2, Math.PI - radian/2, Math.PI, false);
+			context.strokeStyle = color;
+			context.stroke();
+
+			// if (lineRound == true && lineWidth < width) context.lineCap = "round";
+		}
+	});
+})(jQuery);
