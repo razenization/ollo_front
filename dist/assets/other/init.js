75695f4488e9906fdcb327ffb05a0cf62b80be98
razenization <reactionnnnn@gmail.com>
2020-04-13 23:40:29 +0300

    Just a commit. I'm tired.

--- dist/assets/other/init.js
+++ dist/assets/other/init.js
@@ -2,6 +2,7 @@
 	$(function () {
 
 		var btn = $('#button-to-top');
+		var inAnimation = false;
 
         $(window).scroll(function () {
             if ($(window).scrollTop() < 800) {
@@ -23,13 +24,45 @@
 			slidesToScroll: 1,
 			autoplay: true,
 			arrows: true,
-			autoplaySpeed: 2000,
+			autoplaySpeed: 3500,
 		});
 
-		$(".match__main").click(function () {
-			let wrapper = $(this).parents('.match__wrapper');
-			// console.log($(temp).children('.match__main-content'));
-			wrapper.children('.match__main-content').slideToggle();
+		$(window).width()
+
+		$('.lives__main-wrapper').each(function () {
+			console.log($(this));
+			let mInfo = $(this).find('.match__info');
+			mInfo.attr('data-height', mInfo.css('height'));
+			$(this).find('.media').animate({'margin-top': parseInt(mInfo.css('height')) + 25}, 300);
+			$(this).find('.media').css('margin-top', mInfo.css('height'));
+		});
+
+		$(".lives__main-tablerow").click(function () {
+			let wrapper = $(this).parents('.lives__main-wrapper');
+			let matchInfo = wrapper.find('.match__info');
+			let mediaBlock = wrapper.find('.media');
+
+			if (!inAnimation) {
+				inAnimation = true;
+				console.log(mediaBlock.css('margin-top'));
+				if (mediaBlock.css('margin-top') === '0px') {
+					wrapper.find('.media').slideToggle(300);
+					setTimeout(function () {
+						matchInfo.css('display', 'flex');
+						wrapper.find('.media').animate({'margin-top': parseInt(matchInfo.css('height')) + 25}, 300);
+					}, 300);
+				} else {
+					console.log('else');
+					wrapper.find('.media').animate({'margin-top': '0'}, 300);
+					wrapper.find('.media').slideToggle(300);
+					setTimeout(function () {
+						matchInfo.css('display', 'none');
+					}, 300);
+				}
+				setTimeout(function () {
+					inAnimation = false
+				}, 700);
+			}
 		});
     }); // end of document ready
 })(jQuery); // end of jQuery name space
